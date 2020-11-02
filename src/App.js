8a4e6a76477fdad18f44abf0dfca2272a36ce9ef
vigneshpalani003 <vigneshpalani003@gmail.com>
2020-11-02 15:18:00 +0530

    Firebase Working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
+import { db } from "./firebase";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      userName: "Vignesh_BG",
-      caption: "Day 1 learning react",
-      imageUrl:
-        "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
-    },
-    {
-      userName: "Lekhi_Shetty",
-      imageUrl:
-        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*",
-      caption: "My Cutie Pie",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
+
+  // useEffect runs a piece of code based on a specific condition
+
+  useEffect(() => {
+    // This is where the code runs
+    db.collection("posts").onSnapshot((snapshot) => {
+      // Every time a new post is added, this code fires....
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
+    });
+  }, []);
 
   return (
     <div className="App">
@@ -28,9 +31,10 @@ function App() {
         />
       </div>
 
-      {posts.map((post) => {
+      {posts.map(({ id, post }) => {
         return (
           <Post
+            key={id}
             userName={post.userName}
             caption={post.caption}
             imageUrl={post.imageUrl}
